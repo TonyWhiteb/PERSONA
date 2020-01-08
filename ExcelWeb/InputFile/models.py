5dc49f355431ec2005bde413358a5cefd105336c
Whitetony386 <Newbee_tony@outlook.com>
2020-01-08 14:59:08 -0600

    Migration

--- ExcelWeb/InputFile/models.py
+++ ExcelWeb/InputFile/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class InputFile(models.Model):
-    file_name = models.CharField(max)
+    file_name = models.CharField(max_length= 30)
     
