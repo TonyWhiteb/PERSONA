b1f60dfde3afe76d7243e65c30fb25e39c0d37c3
Whitetony386 <Newbee_tony@outlook.com>
2020-01-09 14:35:43 -0600

    registerAPP(admin file)

--- ExcelWeb/InputFile/models.py
+++ ExcelWeb/InputFile/models.py
@@ -3,5 +3,12 @@ from django.db import models
 # Create your models here.
 
 class InputFile(models.Model):
+
+    FILE_TYPE = (
+        ('.xlsx','Excel File'),
+        ('.txt','Text File'),
+        ('.error','Error File'),
+    )    
+
     file_name = models.CharField(max_length= 30)
-    
+    file_type = models.CharField(max_length=10, choices= FILE_TYPE)
