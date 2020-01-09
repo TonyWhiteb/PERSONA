ce369fb3a5a4b1873802ff7fca9d19ba4ed1cfdc
Whitetony386 <Newbee_tony@outlook.com>
2020-01-09 15:16:30 -0600

    Objects(views)

--- ExcelWeb/InputFile/views.py
+++ ExcelWeb/InputFile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import InputFile
 # Create your views here.
 def InputFile_detail(request, file_id):
+    afile = InputFile.objects.get(id = file_id)
     pass
