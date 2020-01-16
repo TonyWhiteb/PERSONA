81207414c4d4aa74db0296cc7f4ae652a03360bb
Bai <bxh0002@dcccd.edu>
2020-01-16 14:43:49 -0600

    Views_templates

--- ExcelWeb/aFile/views.py
+++ ExcelWeb/aFile/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import aFile 
 # Create your views here.
+
+def file_list(request):
+    context = {}
+    context['file'] = aFile.objects.all()
+    return render(request, 'file_list.html', context= context)
+
+def file_upload(request, file_pk):
+    context  = {}
+    context['file'] = get_object_or_404(aFile, pk= file_pk)
+    return render(request, 'file_upload.html', context= context)
