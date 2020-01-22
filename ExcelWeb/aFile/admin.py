0953400ba213f0dde421380d1d40533e3ec8ea45
Whitetony386 <Newbee_tony@outlook.com>
2020-01-22 13:29:52 -0600

    ModifyModel

--- ExcelWeb/aFile/admin.py
+++ ExcelWeb/aFile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import FileFieldForm
 # Register your models here.
+
+
+@admin.register(FileFieldForm)
+class FileFieldFormAdmin(admin.ModelAdmin):
+    list_display = ['title', 'upload_by', 'create_time', 'last_update']
--- ExcelWeb/aFile/models.py
+++ ExcelWeb/aFile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
@@ -15,27 +16,33 @@ from django.utils.translation import gettext_lazy as _
 #         choices=TypeName.choices,
 #         default=TypeName.EXCEL
 #     )    
-
-class FileContent(models.Model):
-    pass
-
-class InputFile(models.Model):
-
-    class TypeName(models.TextChoices):
-        EXCEL = '.xlsx', _('Excel File')
-        ERROR = '.error', _('Error File')
-        TEXT = '.txt', _('Text File')
-
-    file_name = models.CharField(max_length=50)
-    file_type = models.CharField(
-        max_length=2,
-        choices=TypeName.choices,
-        default=TypeName.EXCEL
-    )
+class FileFieldForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
     upload_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    content = models.ForeignKey(FileContent, on_delete=models.DO_NOTHING)
     create_time = models.DateField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '<File: %s>' % self.file_name
+        return '<File: %s>' % self.title
+
+# class InputFile(models.Model):
+
+#     class TypeName(models.TextChoices):
+#         EXCEL = '.xlsx', _('Excel File')
+#         ERROR = '.error', _('Error File')
+#         TEXT = '.txt', _('Text File')
+
+#     file_name = models.CharField(max_length=50)
+#     file_type = models.CharField(
+#         max_length=8,
+#         choices=TypeName.choices,
+#         default=TypeName.EXCEL
+#     )
+#     upload_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+#     content = models.ForeignKey(FileContent, on_delete=models.DO_NOTHING)
+#     create_time = models.DateField(auto_now_add=True)
+#     last_update = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return '<File: %s>' % self.file_name
