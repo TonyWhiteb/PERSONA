72f4ee9e25b79ea2012fd1a9b670039d97888814
Whitetony386 <Newbee_tony@outlook.com>
2020-03-09 16:24:07 -0500

    TEST

--- ExcelWeb/document/views.py
+++ ExcelWeb/document/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,include
+from django.http import JsonResponse
+from django.views import View
 
+from .forms import DocForm
+from .models import Document
 # Create your views here.
+
+class BasicUploadView(View):
+    def get(self, request):
+        file_list = Document.objects.all()
+        return render(self.reqeust, '')
+X
