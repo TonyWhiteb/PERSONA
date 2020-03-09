d3fe41fe777b07c7df1b16b31e8e00c1592c8232
Whitetony386 <Newbee_tony@outlook.com>
2020-03-09 15:23:58 -0500

    ConfigAPP

--- ExcelWeb/document/models.py
+++ ExcelWeb/document/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Document(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    afile = models.FileField(upload_to='docs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    upload_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
