48627c47385c7fb82a5d8584014e496021e01e5d
Whitetony386 <Newbee_tony@outlook.com>
2020-03-09 14:48:27 -0500

    Revert "Models"
    This reverts commit fb1e975abddc6f5bc3c0d9b9a5c7bcf2f96a593b.

--- ExcelWeb/account/models.py
+++ ExcelWeb/account/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
-
-
-class Account(User):
-    
-    pass
-
-class Documents(models.Model):
-    title = models.CharField(max_length=255, blank=True)
-    afile = models.FileField(upload_to='docs/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.DateTimeField(auto_now=True)
